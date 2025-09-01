c3d81c37bf9ea225772332e3c5aad26398694cf3
guto-farias <gutofariasdev@gmail.com>
2025-08-31 21:50:22 -0300

    exe01

---
+++ list-1/exe02.java
@@ -0,0 +1,13 @@
+/*
+ * Sabe-se que o quilowatt de energia custa 1% do salário mínimo. Faça um programa
+    que recebe o valor do salário mínimo e a quantidade de quilowatts consumida por uma
+    residência, calcule e mostre:
+        • o valor de cada quilowatt;
+        • o valor a ser pago por essa residência;
+ */
+
+public class exe02 {
+    public static void main(String[] args){
+
+    }
+}
